668c55ea1ecdce7468570b8fafc804878dca6a98
Klay Garcia <klaywork2005@gmail.com>
2026-09-02 18:00:19 -0500

    footer update

--- templates/src/assets/frontend components/footer.tsx
+++ templates/src/assets/frontend components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="flex justify-between w-full border-t border-mist-400 px-6 py-8 text-center text-sm text-mist-700 lg:w-[76vw] lg:border-x">
             {/* Displays the copyright. */}
             <p>© 2026 Klay Garcia</p>
-            <a href="github.com/klaywork2005/lookback">Documentation</a>
+            <a href="https://github.com/klaywork2005/lookback " target="_blank" rel="noopener noreferrer"  className="hover:underline hover:text-mist-600">Documentation</a>
         {/* Ends the footer. */}
         </footer>
     )
